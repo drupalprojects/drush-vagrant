4b6af39c8bb876aa58b7adafba6f695cbe04747a
Christopher Gervais <chris@ergonlogic.com>
2012-02-25 19:34:51 -0500

    Making sure the correct default base box is selected while I fix up aegir.box

--- settings.rb
+++ settings.rb
@@ -1,18 +1,18 @@
 class Vm                            # default virtual machine settings
   Basebox   = "debian-LAMP-6.0.4"        # default basebox
   Box_url   = "http://ergonlogic.com/files/boxes/debian-LAMP-current.box"
-  Gui       = 1                     # start VM with GUI? Useful for loading CD/DVD ISOs
+  Gui       = false                 # start VM with GUI? Useful for loading CD/DVD ISOs
   Memory    = 512                   # default VM memory
   Manifests = "manifests"           # puppet manifests folder name
   Modules   = "modules"             # puppet modules folder name
   Subnet    = "32"                  # 192.168.###.0/24 subnet for this network
-  Verbose   = 0                     # make output verbose?
-  Debug     = 0                     # output debug info?
+  Verbose   = false                 # make output verbose?
+  Debug     = false                 # output debug info?
   Options   = ""                    # options to pass to Puppet
 end
 
 class Hm                            # settings for our Aegir hostmaster machine
-#  Basebox   = "aegir-1.6.0"         # pre-built Aegir server base box
+#  Basebox   = "aegir-1.6"           # pre-built Aegir server base box
 #  Box_url   = "http://ergonlogic.com/files/boxes/aegir-current.box"
   Shortname = "hm"                  # Vagrant name (also used for manifest name, e.g., hm.pp)
   Hostname  = "aegir.local"         # host FQDN
@@ -22,7 +22,7 @@ end
 
 class Hs                            # settings for our Aegir hostslave machine(s)
 #  Basebox   = "debian-LAMP-6.0.4"   # basic LAMP server base box
-#  Box_url   = "http://ergonlogic.com/files/boxes/debian-stable-LAMP-current.box"
+#  Box_url   = "http://ergonlogic.com/files/boxes/debian-LAMP-current.box"
   Count     = 0                     # number of hostslaves to create (will be used as a suffix to Shortname, Hostname & Vmname)
   Shortname = "hs"                  # Vagrant name (also used for manifest name, e.g., hs.pp)
   Hostname  = "cluster"             # host FQDN
