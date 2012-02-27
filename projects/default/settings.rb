1df289094bbc4ee7f1a4aa7f50a123a139dc08c4
Christopher Gervais <chris@ergonlogic.com>
2012-02-26 21:24:39 -0500

    added second modules/

--- projects/default/settings.rb
+++ projects/default/settings.rb
@@ -4,7 +4,7 @@ class Vm                            # default virtual machine settings
   Gui       = false                 # start VM with GUI? Useful for loading CD/DVD ISOs
   Memory    = 512                   # default VM memory
   Manifests = "manifests"           # puppet manifests folder name
-  Modules   = "modules"             # puppet modules folder name
+  Modules   = [ "modules", "../../lib/modules" ]             # puppet modules folder name
   Subnet    = "32"                  # 192.168.###.0/24 subnet for this network
   Verbose   = false                 # make output verbose?
   Debug     = false                 # output debug info?
