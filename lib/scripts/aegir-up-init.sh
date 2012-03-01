68b46890d2952330020835b88e411a1abcf94363
Christopher Gervais <chris@ergonlogic.com>
2012-02-29 23:16:17 -0500

    avoid domain name collisions, and a bit of cleanup of the init script

--- lib/scripts/aegir-up-init.sh
+++ lib/scripts/aegir-up-init.sh
@@ -32,7 +32,7 @@ prompt_yes_no() {
 ########################################################################
 
 AEGIR_UP_ROOT=$(dirname $0)
-HELP="Usage: $0 [-v|-q] [-n] [-y] [-t TEMPLATE] PROJECT
+HELP="Usage: $0 [-n] [-y] [-t TEMPLATE] PROJECT
 Initialize the PROJECT directory
 
   -t   Specify a template project to use
@@ -42,9 +42,9 @@ Initialize the PROJECT directory
 
 GIT=on
 YES=off
-TEMPLATE="$AEGIR_UP_ROOT/lib/templates/default"
+TEMPLATE="default"
 NEW_PROJECT=
-while getopts vqnyht: opt
+while getopts nyht: opt
 do
     case "$opt" in
       n)  GIT=off;;
@@ -62,8 +62,8 @@ shift `expr $OPTIND - 1`
 
 NEW_PROJECT=$@
 
-if ! [ -d $TEMPLATE ] ; then
-  msg "ERROR: Could not find the $TEMPLATE directory."
+if ! [ -d $AEGIR_UP_ROOT/lib/templates/$TEMPLATE ] ; then
+  msg "ERROR: Could not find the '$TEMPLATE' template."
   exit 1
 fi
 
@@ -74,7 +74,7 @@ if [ -d $AEGIR_UP_ROOT/projects/$NEW_PROJECT ] ; then
 fi
 
 msg "This script will create a new project at $AEGIR_UP_ROOT/projects/$NEW_PROJECT."
-msg "It will use $TEMPLATE as a template."
+msg "It will use the '$TEMPLATE' template."
 if [ "$GIT" = "on" ] ; then
   msg "A Git repo will be initialized in the new project directory."
 fi
@@ -153,8 +153,8 @@ if [ "$TEMPLATE" = "default" ] ; then
 else
   msg "           192.168.$NEW_SUBNET.10    $NEW_PROJECT.aegir.local"
 fi
-msg "You can now: * Alter Aegir-up's behaviour by editing $AEGIR_UP_ROOT/projects/$NEW_PROJECT/settings.rb."
-msg "             * Redefine the VMs by editing the Puppet manifests in $AEGIR_UP_ROOT/projects/$NEW_PROJECT/manifests."
-msg "             * Add additional Puppet modules by copying them to $AEGIR_UP_ROOT/projects/$NEW_PROJECT/modules."
+msg "You can now: * Alter Aegir-up's behaviour by editing '$AEGIR_UP_ROOT/projects/$NEW_PROJECT/settings.rb.'"
+msg "             * Redefine the VMs by editing the Puppet manifests in '$AEGIR_UP_ROOT/projects/$NEW_PROJECT/manifests.'"
+msg "             * Add additional Puppet modules by copying them to '$AEGIR_UP_ROOT/projects/$NEW_PROJECT/modules.'"
 #msg "             * Build platforms in the front-end based on makefiles added to $AEGIR_UP_ROOT/projects/$NEW_PROJECT/makefiles."
 #msg "             * Have platforms built outside the VM by mounting $AEGIR_UP_ROOT/projects/$NEW_PROJECT/platforms under NFS."
