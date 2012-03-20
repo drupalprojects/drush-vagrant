986a40c0f2162bfff4a8889748be6b79870c8975
Christopher Gervais <chris@ergonlogic.com>
2012-03-20 09:16:58 -0400

    Move project setup from clone to shared functions.

--- lib/scripts/aegir-up-clone.sh
+++ lib/scripts/aegir-up-clone.sh
@@ -52,47 +52,11 @@ cd "$AEGIR_UP_ROOT/projects/"
 git clone $GIT_REPO $DIRECTORY
 cd "$AEGIR_UP_ROOT/projects/$DIRECTORY"
 
-# Make project-specific changes 
-ln -s ../../lib/templates/Vagrantfile . 
-ln -s ../../lib/templates/gitignore ./.gitignore 
-sed "s/\"$INITIAL_SUBNET\"/\"$NEW_SUBNET\"/g" -i settings.rb 
-sed "s/\"Aegir\"/\"Aegir($NEW_PROJECT)\"/g" -i settings.rb 
-sed "s/\"Cluster\"/\"Cluster($NEW_PROJECT)\"/g" -i settings.rb 
-if ! [ "$TEMPLATE" = "default" ] ; then 
-  sed "s/\"aegir.local\"/\"$NEW_PROJECT.aegir.local\"/g" -i settings.rb 
-  sed "s/'aegir.local'/'$NEW_PROJECT.aegir.local'/g" -i manifests/hm.pp 
-fi 
- 
-# Get user-specific files & make appropriate changes 
-if [ -e ~/.aegir-up ] ; then 
-  . ~/.aegir-up 
-  USER_DOTFILES_DIR=manifests/files 
-  mkdir $USER_DOTFILES_DIR 
-  cp $PROFILE_PATH $USER_DOTFILES_DIR 
-  cp $BASHRC_PATH $USER_DOTFILES_DIR 
-  cp $BASH_ALIASES_PATH $USER_DOTFILES_DIR 
-  cp $VIMRC_PATH $USER_DOTFILES_DIR 
-  cp $SSH_KEY_PUBLIC_PATH "$USER_DOTFILES_DIR/authorized_keys" 
-  #cp $SSH_KEY_PRIVATE_PATH $USER_DOTFILES_DIR 
-  sed "s/#  \$aegir_up_username = 'username'/  \$aegir_up_username = '$USER_NAME'/g" -i manifests/hm.pp 
-  sed "s/#  \$aegir_up_git_name = 'Firstname Lastname'/  \$aegir_up_git_name = '$GIT_NAME'/g" -i manifests/hm.pp 
-  sed "s/#  \$aegir_up_git_email = 'username@example.com'/  \$aegir_up_git_email = '$GIT_EMAIL'/g" -i manifests/hm.pp 
-else  
-  msg "Skipping user-specific settings. Run lib/scripts/aegir-up-user.sh to initialize a .aegir-up file." 
-fi 
 
-# Provision the VM(s) 
-if [ "$UP" = "on" ]; then 
-  vagrant up 
-  # Set up SSH 
-  vagrant ssh-config > .ssh.conf 
-  sed "s/User vagrant/User $USER_NAME/g" -i .ssh.conf 
-  if ! [ -z $SSH_KEY_PUBLIC_PATH ]; then 
-    sed "s|IdentityFile $HOME/.vagrant.d/insecure_private_key|IdentityFile $HOME/.ssh/id_rsa|g" -i .ssh.conf 
-  fi 
-else 
-  echo "Skipping automatic provisioning." 
-fi 
+setup_new_project
+if [ "$?" -eq "1" ]; then
+  exit 1
+fi
 
 echo "Project successfully cloned." 
 echo "" 
