0facea38a2fcbe26c419740fa3a721d853743fee
Steven Merrill <smerrill@phase2technology.com>
2012-04-04 09:59:29 -0400

    Add variable comments, print <?php in alias output.

--- templates/alias.tpl.php
+++ templates/alias.tpl.php
@@ -1,6 +1,16 @@
 <?php
-// @TODO: Add comments.
+/**
+ * @file
+ * Default implementation to output a drush alias.
+ *
+ * Variables:
+ * - $alias: The name of the alias (user input.)
+ * - $uri: The URI to reach the site (user input.)
+ * - $docroot: The filesystem path to the site's docroot (user input.)
+ * - $ssh_options: The parsed output of `vagrant ssh_config`.
+ */
 ?>
+<?php print "<?php\n\n" ?>
 $aliases['<?php print $alias; ?>'] = array(
   'uri' => '<?php print $uri; ?>',
   'root' => '<?php print $docroot; ?>',
