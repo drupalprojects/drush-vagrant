57224ef3d93b44db1ef2abc51e31013e32f8e020
Christopher Gervais <chris@ergonlogic.com>
2012-04-09 21:35:45 -0400

    Refactoring Vagrantfile and settings.rb to allow arbitrary types of VMs to be defined.

--- lib/blueprints/utility/settings.rb
+++ lib/blueprints/utility/settings.rb
@@ -1,23 +1,27 @@
-class Vm                            # default virtual machine settings
+class Default                            # default virtual machine settings
+  Count     = 1                          # The number of VMs to create
   Basebox   = "debian-LAMP-2012-03-29"   # default basebox
   Box_url   = "http://ergonlogic.com/files/boxes/debian-LAMP-current.box"
-  Gui       = true                 # start VM with GUI? Useful for loading CD/DVD ISOs
-  Memory    = 512                   # default VM memory
-  Manifests = "manifests"           # puppet manifests folder name
-  Modules   = "modules"             # puppet modules folder name
-  Verbose   = true                 # make output verbose?
-  Debug     = true                 # output debug info?
-  Options   = ""                    # options to pass to Puppet
-  Domain    = "util.local"
+  Gui       = false                      # start VM with GUI?
+  Memory    = 512                        # default VM memory
+  Manifests = "manifests"                # puppet manifests folder name
+  Modules   = "modules"                  # puppet modules folder name
+  Verbose   = false                      # make output verbose?
+  Debug     = false                      # output debug info?
+  Options   = ""                         # options to pass to Puppet
+  Address   = 10
+  Domain    = "local"                    # default domain
 end
 
-class Hm                            # settings for our Aegir hostmaster machine
-#  Basebox   = "debian-LAMP-2012-03-29"           # pre-built Aegir server base box
-#  Box_url   = "http://ergonlogic.com/files/boxes/debian-LAMP-current.box"
-  Shortname  = "util"                  # Vagrant name (also used for manifest name, e.g., hm.pp)
-  Vmname     = "Utility"               # VirtualBox name
-  Aegir_root = "/var/aegir"
-  Aegir_user = "aegir"
-#  Memory    = 1024                  # override the default memory
+
+class Hm < Default             # VM-specific overrides of default settings
+  Count      = 2
+  Shortname  = "util"          # Vagrant name (used for manifest name, e.g., hm.pp)
+  Vmname     = "Utility"       # VirtualBox name
+  Aegir_root = "/var/aegir"    # Shared folder(s)
+  Aegir_user = "aegir"         # Shared folder owner in VM
 end
 
+class Vm
+  Types = [ Hm ]
+end
