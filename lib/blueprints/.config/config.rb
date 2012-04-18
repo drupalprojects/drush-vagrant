78f771291470f5e51323120ea3f4effd1076ba95
Christopher Gervais <chris@ergonlogic.com>
2012-04-18 13:29:22 -0400

    Rename Facter vars.

--- lib/blueprints/.config/config.rb
+++ lib/blueprints/.config/config.rb
@@ -2,12 +2,12 @@ require "/path/to/global.rb"
 require "./settings.rb"
 
 class Conf < Global
-  Workspace = "workspace_name"
+  Project   = "project_name"
   Modules   = "vagrant_modules_path"    # puppet modules folder name
   Subnet    = "subnet"                  # 192.168.###.0/24 subnet for this network
 end
 
-class AegirUpUser
+class DrushVagrantUser
   Username  = "username"
   Uid       = "uid"
   Gid       = "gid"
