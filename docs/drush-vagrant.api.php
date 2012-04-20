0e220a44cfbd6acb2a80d7fc9dc6e16cd3ce22c0
Christopher Gervais <chris@ergonlogic.com>
2012-04-20 13:26:31 -0400

    Add command to list blueprints, and allow blueprints to provide extensible info.

--- docs/drush-vagrant.api.php
+++ docs/drush-vagrant.api.php
@@ -5,12 +5,20 @@
  *
  * @return
  *   Return an array of blueprints, where the key is the blueprint name and the
- *   value is the (relative) path to the blueprint itself.
+ *   value is an extensible list of information relating to the blueprint.
  */
 function hook_vagrant_blueprints() {
   $blueprints = array(
-    'blueprint' => 'path/to/blueprint',
-    'other'     => 'path/to/other',
-  );
+    'default' => array(
+      'name' => 'Default',
+      'description' => 'The default blueprint.',
+      'path' => 'blueprints/default', // req'd
+      ),
+    'another' => array(
+      'name' => 'Second',
+      'description' => 'Another blueprint.',
+      'path' => 'blueprints/another', // req'd
+      ),
+    );
   return $blueprints;
 }
