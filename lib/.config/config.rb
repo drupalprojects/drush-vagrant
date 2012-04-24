3a26920f1a98819d766369be9ed6d245ebd400f6
Christopher Gervais <chris@ergonlogic.com>
2012-04-24 10:57:28 -0400

    Switch to using hashes for Facts.

--- lib/.config/config.rb
+++ lib/.config/config.rb
@@ -5,16 +5,14 @@ class Conf < Global
   Project   = "project_name"
   Modules   = "vagrant_modules_path"    # puppet modules folder name
   Subnet    = "subnet"                  # 192.168.###.0/24 subnet for this network
-  Facts     = [
-              # Fix for broken $fqdn fact on Debian
-              [ "fqdn",               $hostname ],
-              # Add user variables to be processed in drush-vagrant::user
-              [ "drush_vagrant_username",  "username" ],
-              [ "drush_vagrant_git_name",  "Firstname Lastname" ],
-              [ "drush_vagrant_git_email", "username@example.com" ],
-              # For NFS, these can be used to match uid/gid to the host user
-              [ "drush_vagrant_uid",       "uid" ],
-              [ "drush_vagrant_gid",       "gid" ],
-              ] 
+  Facts     = { # Fix for broken $fqdn fact on Debian
+                "fqdn"                    => $hostname,
+                # Add user variables to be processed in drush-vagrant::user
+                "drush_vagrant_username"  => "username",
+                "drush_vagrant_git_name"  => "Firstname Lastname",
+                "drush_vagrant_git_email" => "username@example.com",
+                # For NFS, these can be used to match uid/gid to the host user
+                "drush_vagrant_uid"       => "uid",
+                "drush_vagrant_gid"       => "gid",
+              } 
 end
-
