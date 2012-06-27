b7a2966a7d227e2eed5593ab0cb7eb891664dc67
Christopher Gervais <chris@ergonlogic.com>
2012-06-27 10:16:30 -0400

    Generate hashes of facts and modules in config template.

--- templates/config.tpl.php
+++ templates/config.tpl.php
@@ -4,14 +4,11 @@
  * Default implementation to output a Drush Vagrant config file.
  *
  * Variables:
- * - $global_path: The filesystem path to global.rb config file
- * - $project_name: The name of the Vagrant project
+ * - $global_path: The filesystem path to global.rb config file.
+ * - $project_name: The name of the Vagrant project.
  * - $subnet: The /24 subnet the project will use.
- * - $username: The user's system username
- * - $git_name: The user's full name, as registered with Git
- * - $git_email: The user's email address, as registered with Git
- * - $uid: The user's system user ID, used for NFS
- * - $gid: The user's system group ID, used for NFS
+ * - $facts: An array of Facter facts.
+ * - $modules: An array of directories to look for Puppet modules in.
  */
 ?>
 require "<?php print $global_path; ?>"
@@ -20,14 +17,20 @@ require "./settings.rb"
 class Conf < Global
   Project   = "<?php print $project_name; ?>"
   Subnet    = "<?php print $subnet; ?>"                  # 192.168.###.0/24 subnet for this network
-  Facts     = { # Fix for broken $fqdn fact on Debian
-                "fqdn"                    => $hostname,
-                # Add user variables to be processed in drush-vagrant::user
-                "drush_vagrant_username"  => "<?php print $username; ?>",
-                "drush_vagrant_git_name"  => "<?php print $git_name; ?>",
-                "drush_vagrant_git_email" => "<?php print $git_email; ?>",
-                # For NFS, these can be used to match uid/gid to the host user
-                "drush_vagrant_uid"       => "<?php print $uid; ?>",
-                "drush_vagrant_gid"       => "<?php print $gid; ?>",
-              }
+  <?php
+    if (isset($facts)) {
+      print "Facts     = {\n";
+      foreach ($facts as $name => $fact) {
+        print '                "' . $name . '" => ' . $fact . ",\n";
+      }
+      print "              }\n";
+    }
+    if (isset($modules)) {
+      print "  Modules   = {\n";
+      foreach ($modules as $name => $dir) {
+        print '                "' . $name . '" => "' . $dir . "\",\n";
+      }
+      print "              }\n";
+    }
+  ?>
 end
