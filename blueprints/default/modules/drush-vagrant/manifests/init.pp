1249df76a7bf823ab517f20adf7145f1892aebe3
Christopher Gervais <chris@ergonlogic.com>
2012-07-04 11:20:29 -0400

    Fix Facter variable names in custom puppet module.

--- blueprints/default/modules/drush-vagrant/manifests/init.pp
+++ blueprints/default/modules/drush-vagrant/manifests/init.pp
@@ -2,55 +2,55 @@ class drush-vagrant::user {
 
   group { 'puppet': ensure => present, }
 
-  user {"${drush_vagrant_username}":
+  user {$username:
     ensure => present,
     groups => 'sudo',
-    home   => "/home/${drush_vagrant_username}",
+    home   => "/home/${username}",
     shell  => '/bin/bash',
   }
 
   # Various dotfiles
   File { ensure => present,
-         owner  => "${drush_vagrant_username}",
-         group  => "${drush_vagrant_username}",
+         owner  => "${username}",
+         group  => "${username}",
   }
-  file { "/home/${drush_vagrant_username}":
+  file { "/home/${username}":
            ensure => directory,
-           before => [ File['.profile'], File['.bashrc'], File['.bash_aliases'], File['.vimrc'], File["/home/${drush_vagrant_username}/.ssh"] ];
-         "/home/${drush_vagrant_username}/.ssh":
+           before => [ File['.profile'], File['.bashrc'], File['.bash_aliases'], File['.vimrc'], File["/home/${username}/.ssh"] ];
+         "/home/${username}/.ssh":
            ensure => directory,
            before => File['.ssh/authorized_keys'];
          ".profile":
            source => "/vagrant/.config/files/.profile",
-           path   => "/home/${drush_vagrant_username}/.profile";
+           path   => "/home/${username}/.profile";
          ".bashrc":
            source => "/vagrant/.config/files/.bashrc",
-           path   => "/home/${drush_vagrant_username}/.bashrc";
+           path   => "/home/${username}/.bashrc";
          ".bash_aliases":
            source => "/vagrant/.config/files/.bash_aliases",
-           path   => "/home/${drush_vagrant_username}/.bash_aliases";
+           path   => "/home/${username}/.bash_aliases";
          ".vimrc":
            source => "/vagrant/.config/files/.vimrc",
-           path   => "/home/${drush_vagrant_username}/.vimrc";
+           path   => "/home/${username}/.vimrc";
          ".ssh/authorized_keys":
            source => "/vagrant/.config/files/authorized_keys",
-           path   => "/home/${drush_vagrant_username}/.ssh/authorized_keys";
+           path   => "/home/${username}/.ssh/authorized_keys";
   }
 
   #git username & email
   include git
 
-  Exec { user        => "${drush_vagrant_username}",
-         group       => "${drush_vagrant_username}",
-         environment => "HOME=/home/${drush_vagrant_username}",
+  Exec { user        => $username,
+         group       => $username,
+         environment => "HOME=/home/${username}",
          path        => '/usr/bin',
          require     => Class['git'],
   }
-  if $drush_vagrant_git_name {
-    exec {"git config --global user.name '${drush_vagrant_git_name}'":}
+  if $git_name {
+    exec {"git config --global user.name '${git_name}'":}
   }
-  if $drush_vagrant_git_email {
-    exec {"git config --global user.email ${drush_vagrant_git_email}":}
+  if $git_email {
+    exec {"git config --global user.email ${git_email}":}
   }
 
 }
