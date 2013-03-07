28d02e616cd9359b0e38d4f2d8abe70db60b814c
Christopher Gervais <chris@ergonlogic.com>
2013-03-07 13:21:46 -0500

    Revert drush-vagrant puppet module.

--- lib/puppet-modules/drush-vagrant/manifests/init.pp
+++ lib/puppet-modules/drush-vagrant/manifests/init.pp
@@ -1,42 +1,12 @@
-class drush-vagrant::user ($user = [
-  {'name' => $username, 'home_dir' => "/home/$username"},
-  {'name' => 'root',    'home_dir' => '/root'},
-  {'name' => 'vagrant', 'home_dir' => '/home/vagrant'},
-  {'name' => 'aegir',   'home_dir' => '/var/aegir'},
-  ]) {
+class drush-vagrant::user {
 
   group { 'puppet': ensure => present, }
 
-  drush-vagrant::user_account {$user[0]['name']:
-    username => $user[0]['name'],
-    home_dir => $user[0]['home_dir'],
-  }
-  drush-vagrant::user_account {$user[1]['name']:
-    username => $user[1]['name'],
-    home_dir => $user[1]['home_dir'],
-  }
-  drush-vagrant::user_account {$user[2]['name']:
-    username => $user[2]['name'],
-    home_dir => $user[2]['home_dir'],
-  }
-  drush-vagrant::user_account {$user[3]['name']:
-    username => $user[3]['name'],
-    home_dir => $user[3]['home_dir'],
-  }
-
-}
-
-define drush-vagrant::user_account ($username, $home_dir = "/home/$username") {
-
-  User { ensure => present,
-         groups => 'sudo',
-         shell  => '/bin/bash',
-  }
-
-  if !defined(User[$username]) {
-    user {$username:
-      home   => $home_dir,
-    }
+  user {$username:
+    ensure => present,
+    groups => 'sudo',
+    home   => "/home/${username}",
+    shell  => '/bin/bash',
   }
 
   # Various dotfiles
@@ -44,39 +14,31 @@ define drush-vagrant::user_account ($username, $home_dir = "/home/$username") {
          owner  => "${username}",
          group  => "${username}",
   }
-  if !defined(File[$home_dir]) {
-    file { $home_dir:
-             ensure => directory,
-             before => [
-               File["${home_dir}/.profile"],
-               File["${home_dir}/.bashrc"],
-               File["${home_dir}/.bash_aliases"],
-               File["${home_dir}/.vimrc"],
-               File["${home_dir}/.ssh"]
-             ];
-    }
-  }
-  file { "${home_dir}/.ssh":
-           ensure => directory;
-         "${home_dir}/.profile":
+  file { "/home/${username}":
+           ensure => directory,
+           before => [ File['.profile'], File['.bashrc'], File['.bash_aliases'], File['.vimrc'], File["/home/${username}/.ssh"] ];
+         "/home/${username}/.ssh":
+           ensure => directory,
+           before => File['.ssh/authorized_keys'];
+         ".profile":
            source => ["/vagrant/.config/files/.profile",
-                      "puppet:///modules/drush-vagrant/profile.example"];
-         "${home_dir}/.bashrc":
+                      "puppet:///modules/drush-vagrant/profile.example"],
+           path   => "/home/${username}/.profile";
+         ".bashrc":
            source => ["/vagrant/.config/files/.bashrc",
-                      "puppet:///modules/drush-vagrant/bashrc.example"];
-         "${home_dir}/.bash_aliases":
+                      "puppet:///modules/drush-vagrant/bashrc.example"],
+           path   => "/home/${username}/.bashrc";
+         ".bash_aliases":
            source => ["/vagrant/.config/files/.bash_aliases",
-                      "puppet:///modules/drush-vagrant/bash_aliases.example"];
-         "${home_dir}/.vimrc":
+                      "puppet:///modules/drush-vagrant/bash_aliases.example"],
+           path   => "/home/${username}/.bash_aliases";
+         ".vimrc":
            source => ["/vagrant/.config/files/.vimrc",
-                      "puppet:///modules/drush-vagrant/vimrc.example"];
-  }
-
-  if $username != 'vagrant' {
-    file { "${home_dir}/.ssh/authorized_keys":
-             source  => "/vagrant/.config/files/authorized_keys",
-             require => File["${home_dir}/.ssh"];
-    }
+                      "puppet:///modules/drush-vagrant/vimrc.example"],
+           path   => "/home/${username}/.vimrc";
+         ".ssh/authorized_keys":
+           source => "/vagrant/.config/files/authorized_keys",
+           path   => "/home/${username}/.ssh/authorized_keys";
   }
 
   #git username & email
@@ -84,19 +46,15 @@ define drush-vagrant::user_account ($username, $home_dir = "/home/$username") {
 
   Exec { user        => $username,
          group       => $username,
-         environment => "HOME=${home_dir}",
+         environment => "HOME=/home/${username}",
          path        => '/usr/bin',
          require     => Class['git'],
   }
   if $git_name {
-    exec {"git user.name config for $username":
-      command => "git config --global user.name '${git_name}'",
-    }
+    exec {"git config --global user.name '${git_name}'":}
   }
   if $git_email {
-    exec {"git user.email config for $username":
-      command => "git config --global user.email ${git_email}",
-    }
+    exec {"git config --global user.email ${git_email}":}
   }
 
 }
