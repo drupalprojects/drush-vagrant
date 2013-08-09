08ca35595e980e6d6a8c7ee2c1a4deb06a7541cf
Christopher Gervais <chris@ergonlogic.com>
2013-08-09 14:40:45 -0400

    Fix user class and defines.

--- lib/puppet-modules/drush-vagrant/manifests/init.pp
+++ lib/puppet-modules/drush-vagrant/manifests/init.pp
@@ -1,60 +1,107 @@
-class drush-vagrant::user {
+class drush-vagrant::users ($users = '') {
 
-  group { 'puppet': ensure => present, }
+  if $users != '' {
+    drush-vagrant::user { [$users]: }
+  }
+  drush-vagrant::user {'vagrant': }
+  drush-vagrant::user {'root': home_dir => '/root', }
 
-  user {$username:
-    ensure => present,
-    groups => 'sudo',
-    home   => "/home/${username}",
-    shell  => '/bin/bash',
+}
+
+define drush-vagrant::user ($home_dir = '') {
+  # We can't retrieve the resource name in the paramaters themselves to define
+  # a default parameter. That is, the following doesn't work, despite indica-
+  # tions that it should in the Puppet docs:
+  # define drush-vagrant::user ($home_dir = "/home/$name")
+  # define drush-vagrant::user ($home_dir = "/home/$title")
+  # ... So, we use a wrapper definition, and then pass along the $name in the home_dir, if required.
+  if ($home_dir == '') {
+    $home = "/home/${name}"
+  }
+  else {
+    $home = $home_dir
+  }
+  drush-vagrant::user_account {$name:
+    home_dir => $home,
+  }
+}
+
+define drush-vagrant::user_account ($home_dir) {
+
+  User { ensure => present,
+         groups => 'sudo',
+         shell  => '/bin/bash',
+  }
+
+  if !defined(User[$name]) {
+    user {$name:
+      home   => $home_dir,
+    }
   }
 
   # Various dotfiles
   File { ensure => present,
-         owner  => "${username}",
-         group  => "${username}",
+         owner  => $name,
+         group  => $name,
   }
-  file { "/home/${username}":
-           ensure => directory,
-           before => [ File['.profile'], File['.bashrc'], File['.bash_aliases'], File['.vimrc'], File["/home/${username}/.ssh"] ];
-         "/home/${username}/.ssh":
-           ensure => directory,
-           before => File['.ssh/authorized_keys'];
-         ".profile":
+  if !defined(File[$home_dir]) {
+    file { $home_dir:
+             ensure => directory,
+             before => [
+               File["${home_dir}/.profile"],
+               File["${home_dir}/.bashrc"],
+               File["${home_dir}/.bash_aliases"],
+               File["${home_dir}/.vimrc"],
+               File["${home_dir}/.ssh"]
+             ];
+    }
+  }
+  if !defined(File["${home_dir}/.ssh"]) {
+    file { "${home_dir}/.ssh":
+      ensure => directory;
+    }
+  }
+  file { "${home_dir}/.profile":
            source => ["/vagrant/.config/files/.profile",
-                      "puppet:///modules/drush-vagrant/profile.example"],
-           path   => "/home/${username}/.profile";
-         ".bashrc":
+                      "puppet:///modules/drush-vagrant/profile.example"];
+         "${home_dir}/.bashrc":
            source => ["/vagrant/.config/files/.bashrc",
-                      "puppet:///modules/drush-vagrant/bashrc.example"],
-           path   => "/home/${username}/.bashrc";
-         ".bash_aliases":
+                      "puppet:///modules/drush-vagrant/bashrc.example"];
+         "${home_dir}/.bash_aliases":
            source => ["/vagrant/.config/files/.bash_aliases",
-                      "puppet:///modules/drush-vagrant/bash_aliases.example"],
-           path   => "/home/${username}/.bash_aliases";
-         ".vimrc":
+                      "puppet:///modules/drush-vagrant/bash_aliases.example"];
+         "${home_dir}/.vimrc":
            source => ["/vagrant/.config/files/.vimrc",
-                      "puppet:///modules/drush-vagrant/vimrc.example"],
-           path   => "/home/${username}/.vimrc";
-         ".ssh/authorized_keys":
-           source => "/vagrant/.config/files/authorized_keys",
-           path   => "/home/${username}/.ssh/authorized_keys";
+                      "puppet:///modules/drush-vagrant/vimrc.example"];
+  }
+
+  if $name != 'vagrant' {
+    file { "${home_dir}/.ssh/authorized_keys":
+             source  => "/vagrant/.config/files/authorized_keys",
+             require => File["${home_dir}/.ssh"];
+    }
   }
 
   #git username & email
-  include git
+  if !defined(Package['git']) {
+    package { 'git': }
+  }
 
-  Exec { user        => $username,
-         group       => $username,
-         environment => "HOME=/home/${username}",
+  Exec { user        => $name,
+         group       => $name,
+         environment => "HOME=${home_dir}",
          path        => '/usr/bin',
-         require     => Class['git'],
+         require     => Package['git'],
   }
   if $git_name {
-    exec {"git config --global user.name '${git_name}'":}
+    exec {"git user.name config for ${name}":
+      command => "git config --global user.name \"${git_name}\"",
+    }
   }
   if $git_email {
-    exec {"git config --global user.email ${git_email}":}
+    exec {"git user.email config for ${name}":
+      command => "git config --global user.email ${git_email}",
+    }
   }
 
 }
