5a22c2a06b810da64831a9067e8264cf97f0d83c
Christopher Gervais <chris@ergonlogic.com>
2013-08-17 15:44:16 -0400

    Fix other path to test script.

--- lib/puppet-modules/drush_vagrant/tests/.ci/vagrant_test.sh
+++ lib/puppet-modules/drush_vagrant/tests/.ci/vagrant_test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 cd /vagrant
-chmod a+x tests/.ci/test.sh
-./tests/.ci/test.sh
+chmod a+x lib/puppet-modules/drush_vagrant/tests/.ci/test.sh
+./lib/puppet-modules/drush_vagrant/tests/.ci/test.sh
