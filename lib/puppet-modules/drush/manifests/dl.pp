bae629825d13ecb097c974d007b07a6683524a47
Christopher Gervais <chris@ergonlogic.com>
2013-08-29 15:05:23 -0400

    Update puppet-drush.

--- lib/puppet-modules/drush/manifests/dl.pp
+++ lib/puppet-modules/drush/manifests/dl.pp
@@ -1,33 +1,38 @@
 define drush::dl (
   $type       = 'module',
-  $version    = '',
   $site_alias = $drush::defaults::site_alias,
   $options    = $drush::defaults::options,
   $arguments  = $drush::defaults::arguments,
-  $site_path  = $drush::defaults::site_path,
   $drush_user = $drush::defaults::drush_user,
   $drush_home = $drush::defaults::drush_home,
   $log        = $drush::defaults::log
   ) {
 
-  if $version { $real_ver = "-${version}"}
-
   if $arguments { $real_args = $arguments }
-  else { $real_args = "${name}${real_ver}" }
+  else { $real_args = "${name}" }
 
   drush::run {"drush-dl:${name}":
     command    => 'pm-download',
     site_alias => $site_alias,
     options    => $options,
     arguments  => $real_args,
-    site_path  => $site_path,
     drush_user => $drush_user,
     drush_home => $drush_home,
     log        => $log,
   }
 
-  if $site_path {
-    Drush::Run["drush-dl:${name}"] { creates => "${site_path}/${type}s/${name}"}
+  # Add an 'unless' argument depending on the project type.
+  case $type {
+    'module', 'theme': {
+      Drush::Run["drush-dl:${name}"] {
+        unless => "drush ${site_alias} pm-list | grep ${name}",
+      }
+    }
+    'extension': {
+      Drush::Run["drush-dl:${name}"] {
+        unless => "[ -d '${drush_home}/.drush/${name}' ]",
+      }
+    }
   }
 
   if defined(Drush::Run["drush-en:${name}"]) {
